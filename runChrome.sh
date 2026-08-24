d57499f8d2284d54db38fc2631e3bd41635f55da
Andy <qrt9zblng@.gmail.com>
2026-08-24 21:20:28 +0800

    add ShellScript

---
+++ runChrome.sh
@@ -0,0 +1,4 @@
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
+   --remote-debugging-port=9222 \
+   --user-data-dir="$HOME/selenium-chrome-profile" \
+   --disable-features=BackForwardCache
