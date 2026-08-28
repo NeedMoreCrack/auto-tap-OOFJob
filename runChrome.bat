19afae6f10810728141c869fdcf1b62761b3f151
Andy <qrt9zblng@gmail.com>
2026-08-28 22:54:43 +0800

    edit batchSell

--- runChrome.bat
+++ runChrome.bat
@@ -1,6 +1,9 @@
 @echo off
-echo 啟動Google Chrome...
-& "C:\Program Files\Google\Chrome\Application\chrome.exe" `
-   --remote-debugging-port=9222 `
-   --user-data-dir="C:\Users\User\selenium-chrome-profile" `
-   --disable-features=BackForwardCache
+chcp 65001 >nul
+
+echo 啟動 Google Chrome...
+
+"C:\Program Files\Google\Chrome\Application\chrome.exe" ^
+    --remote-debugging-port=9222 ^
+    --user-data-dir="C:\Users\User\selenium-chrome-profile" ^
+    --disable-features=BackForwardCache
